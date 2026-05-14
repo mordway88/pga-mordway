278f5304e901f5d334b48d9b35514477d1d28c4b
Matt Ordway <mattordway@Matts-MacBook-Air-2.local>
2026-05-14 12:53:47 -0700

    Correct Aronimink course pars

--- src/data/coursePars.js
+++ src/data/coursePars.js
@@ -1,4 +1,5 @@
-export const COURSE_PARS = [4, 4, 4, 4, 3, 4, 4, 5, 4, 4, 4, 3, 4, 5, 4, 3, 4, 4];
+// Aronimink Golf Club, 2026 PGA Championship setup: par 70, 35 out / 35 in.
+export const COURSE_PARS = [4, 4, 4, 4, 3, 4, 4, 3, 5, 4, 4, 4, 4, 3, 4, 5, 3, 4];
 
 export const OUT_PAR = COURSE_PARS.slice(0, 9).reduce((sum, par) => sum + par, 0);
 export const IN_PAR = COURSE_PARS.slice(9).reduce((sum, par) => sum + par, 0);
